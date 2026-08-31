fb110353c545d5918a8a6c57b4b0876e5cf8151b
A-NxZI <aryaaldy2009@gmail.com>
2026-08-31 18:32:48 +0700

    edit data base

--- Data_Mahasiswa_/database.sql
+++ Data_Mahasiswa_/database.sql
@@ -12,9 +12,3 @@ CREATE TABLE IF NOT EXISTS mahasiswa (
     telp VARCHAR(50) DEFAULT '-'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Data contoh sesuai buku (halaman 181)
-INSERT INTO mahasiswa (no_induk, nama, jk, tgl_lahir, alamat, dosen, telp) VALUES
-('160202340010', 'Agus',  'L', '1990-10-02', 'Malang',   'Bambang',   '081233456789'),
-('160202340013', 'Budi',  'L', '1989-02-03', 'Surabaya', 'Atin',      '081145673456'),
-('160202340017', 'Cahya', 'P', '1991-05-06', 'Blitar',   'Atin',      '081334556689'),
-('160202340018', 'Doni',  'L', '1990-12-05', 'Malang',   'Samsuddin', '-');
